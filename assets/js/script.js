5960678c265e55f02eb933ab32878c83baae6bdc
Suzana dos Santos <94690066+suzanadossantos@users.noreply.github.com>
2022-10-19 19:22:04 -0300

    iniciando projeto

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Menu
+
+itens.style.display = 'none'
+
+function menu(){
+    if(itens.style.display == 'flex'){
+        itens.style.display = 'none'
+    }else{
+        itens.style.display = 'flex'
+    }
+}
